e32169c3b884156df44cca9b046d0e27f815bb78
Gadi Lifshitz <gadi@safebreach.com>
2017-04-05 22:41:03 +0300

    Initial polling, using Timer to keep things simple.

---
+++ client/Poller.py
@@ -0,0 +1,32 @@
+from threading import Timer
+
+
+class Poller:
+    def __init__(self):
+        print("poller init")
+        self.enabled = False
+        self.t = None
+
+
+    def state(self):
+        return self.enabled
+
+    def poll(self):
+        print("polling")
+        if self.enabled:
+            self.start()
+
+    def setstate(self, enabled):
+        self.enabled = enabled in ['true', 'True', 'yes', 'Yes', '1']
+        if self.enabled:
+            self.start()
+        else:
+            self.stop()
+
+    def start(self):
+        self.t = Timer(5, self.poll)
+        self.t.start()
+
+    def stop(self):
+        if self.t is not None:
+            self.t.cancel()
--- client/main.py
+++ client/main.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from flask import request
 import common.config
+from client.Poller import Poller
 
 config = common.config.config()
 app = Flask(__name__)
+poller = Poller()
+
 
 @app.route("/")
 def hello():
     return "diag client v0.1"
+
+
+@app.route("/state")
+def state():
+    print(request.args.get('enabled'))
+    if request.args.get('enabled') is not None:
+        poller.setstate(request.args.get('enabled'))
+    return str(poller.state())
 
 if __name__ == "__main__":
     app.run(port=config["client"]["port"])
