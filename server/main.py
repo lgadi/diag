e1b087dcd56ae768d943dc3d234414deddaf8e7c
Gadi Lifshitz <gadi@safebreach.com>
2017-04-18 12:45:26 +0300

    added cors support. Also, initial Vue.js working.

--- server/main.py
+++ server/main.py
@@ -14,6 +14,52 @@ config = Config().config
 app = Flask(__name__)
 command_manager = CommandManager()
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 @app.route("/")
 def hello():
@@ -29,6 +75,7 @@ def add_command(client_id):
     return "ok"
 
 @app.route("/client/<client_id>/list")
+@crossdomain(origin='*')
 def list(client_id):
     logger.debug('got list request for client %s' % client_id)
     json_result = json.dumps(command_manager.get_all_commands_for_client(client_id))
